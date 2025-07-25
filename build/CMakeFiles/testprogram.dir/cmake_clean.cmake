6dff26f497105fa7f4af7a4e53f8eb2e986ac822
Lucas <lucaswihren0402@gmail.com>
2025-07-25 16:39:12 +0200

    hello world

---
+++ build/CMakeFiles/testprogram.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testprogram.dir/hello_world.cpp.obj"
+  "CMakeFiles/testprogram.dir/hello_world.cpp.obj.d"
+  "libtestprogram.dll.a"
+  "testprogram.exe"
+  "testprogram.exe.manifest"
+  "testprogram.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testprogram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
